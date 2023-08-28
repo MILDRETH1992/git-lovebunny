f1dc46b7333e0d71c14cc7bc6c56b08f63677237
Mile <salomile1421@gmial.com>
2023-08-28 10:10:12 -0500

    cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './layout/publico/login/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterLinkActive } from '@angular/router';
+import { PrincipalComponent } from './layout/privado/principal/principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PrincipalComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
