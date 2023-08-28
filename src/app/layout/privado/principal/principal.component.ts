b52333fe041bb8bd24501ec6407b926c843157e8
Mile <salomile1421@gmial.com>
2023-08-28 10:33:08 -0500

    Actualizacion

---
+++ src/app/layout/privado/principal/principal.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { AutenticacionService } from 'src/app/shared/servicios/autenticacion.service';
+
+@Component({
+  selector: 'app-principal',
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.css']
+})
+export class PrincipalComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+
+
+  }
+
+}
---
+++ src/app/shared/servicios/autenticacion.service.ts
@@ -0,0 +1,21 @@
+import { ReturnStatement } from '@angular/compiler';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AutenticacionService {
+  private ingresar:boolean = false;
+
+  constructor() { }
+
+  public ingresarAplicativo(obj:any):boolean{
+    this.ingresar = obj.usuario == 'Mile' && obj.password=='123';
+      return this.ingresar;
+  }
+
+  public habilitarlogeo(){
+    return this.ingresar;
+  }
+
+}
